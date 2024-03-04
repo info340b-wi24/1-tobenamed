1d5e5f22cf764ec8782384c79350775cb39b8cf5
AnikaYechuri <anikay@uw.edu>
2024-03-04 08:55:01 -0800

    search images mapped

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './HomePage';
 import QuizPage from './QuizPage';
+import SearchBar from './SearchBar';
 
 export function App () {
   return (
@@ -10,6 +11,8 @@ export function App () {
       <Routes>
         <Route exact path="/" component={HomePage} />
         <Route path="/quiz" component={QuizPage} />
+        <Route path="/searchBar" component={SearchBar} /> {/* Add route for SearchPage */}
+
       </Routes>
     </Router>
   );
