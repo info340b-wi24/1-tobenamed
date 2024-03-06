72ff54c21fe9307fcada94904a922bacbcd8b95b
ASABANOVIC26 <SABANOVIC2002@GMAIL.COM>
2024-03-05 16:50:56 -0800

    fixed nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { Navbar, Nav } from 'react-bootstrap';
 
 export function NavBar() {
- return (
-   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-     <Link className="navbar-brand" to="/">Scent Seeker</Link>
-     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-       <span className="navbar-toggler-icon"></span>
-     </button>
-     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-       <div className="navbar-nav">
-         {/* <Link className="nav-item nav-link" to="/home">Home</Link> */}
-         <Link className="nav-item nav-link" to="/search">Search</Link>
-         <Link className="nav-item nav-link" to="/quiz">Quiz</Link>
-         <Link className="nav-item nav-link" to="/my-list">My List</Link>
-       </div>
-     </div>
-   </nav>
- );
+  return (
+    <Navbar bg="light" expand="lg">
+      <Link className="navbar-brand" to="/">Scent Seeker</Link>
+      <Navbar.Toggle aria-controls="navbarNavAltMarkup" />
+      <Navbar.Collapse id="navbarNavAltMarkup">
+        <Nav className="ml-auto">
+          <Link className="nav-link" to="/search">Search</Link>
+          <Link className="nav-link" to="/quiz">Quiz</Link>
+          <Link className="nav-link" to="/my-list">My List</Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
-
 
 export default NavBar;
